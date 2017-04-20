91fdc5f5ea1e28fecee29edd87897a5218b78a06
elevenBeans <elevenbeansf2e@gmail.com>
2017-04-20 15:04:02 +0800

    test it

--- index.js
+++ index.js
@@ -2,9 +2,24 @@ var express = require('express')
 var app = express()
 
 app.get('/:time', function (req, res) {
-
+  var date;
+  var time = req.params.time;
+  var result = {};
   // console.log(new Date(time*1000));
-
+  
+  if(/\d{10}/.test(time)){
+    date = new Date(time*1000);
+    result.natural = date;
+    result.unix = parseInt(time);
+  } else if (typeof(new Date(time)) === 'object'){
+    date = new Date(time).getTime();
+    result.natural = time;
+    result.unix = date/1000;
+  } else {
+    result.natural = null;
+    result.unix = null;
+  }
+  // console.log(date);
   res.send(result);
 })
 
